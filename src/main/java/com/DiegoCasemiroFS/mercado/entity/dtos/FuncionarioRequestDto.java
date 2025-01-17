74629abaed512b23beea46fda231e3851fb7101d
Diego Casemiro <dcfs1998@gmail.com>
2025-01-17 11:41:09 -0300

    adicionando anotações

--- src/main/java/com/DiegoCasemiroFS/mercado/entity/dtos/FuncionarioRequestDto.java
+++ src/main/java/com/DiegoCasemiroFS/mercado/entity/dtos/FuncionarioRequestDto.java
@@ -2,7 +2,13 @@ package com.DiegoCasemiroFS.mercado.entity.dtos;
 
 import com.DiegoCasemiroFS.mercado.entity.Endereco;
 import com.DiegoCasemiroFS.mercado.entity.enums.Funcao;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class FuncionarioRequestDto {
 
     private String telefone;
