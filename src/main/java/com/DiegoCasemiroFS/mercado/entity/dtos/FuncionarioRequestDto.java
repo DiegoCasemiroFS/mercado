554e89d338730d9a0fc1df77ad99dc083c782781
Diego Casemiro <dcfs1998@gmail.com>
2025-01-17 11:02:25 -0300

    criando classe

---
+++ src/main/java/com/DiegoCasemiroFS/mercado/entity/dtos/FuncionarioRequestDto.java
@@ -0,0 +1,13 @@
+package com.DiegoCasemiroFS.mercado.entity.dtos;
+
+import com.DiegoCasemiroFS.mercado.entity.Endereco;
+import com.DiegoCasemiroFS.mercado.entity.enums.Funcao;
+
+public class FuncionarioRequestDto {
+
+    private String telefone;
+
+    private Endereco endereco;
+
+    private Funcao funcao;
+}
